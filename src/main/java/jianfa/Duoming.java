aef7b74013ffcc1c46261f1ed7fb1302891e8d08
JPF <1101077605@qq.com>
2021-03-16 15:42:21 +0800

    sanjian

---
+++ src/main/java/jianfa/Duoming.java
@@ -0,0 +1,9 @@
+package jianfa;
+
+public class Duoming {
+    public static void main(String[] args) {
+        System.out.println("夺命三剑");
+        System.out.println("夺命三剑");
+        System.out.println("夺命三剑");
+    }
+}
